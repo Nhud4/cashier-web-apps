27ed3bc5537763ecc8c4d0d86e80a45be57416e9
Muhammad Nurul Huda <nu12ulhud4@gmail.com>
2026-01-25 12:25:00 +0700

    feat: add button

--- src/features/CardMenu/CardMenu.tsx
+++ src/features/CardMenu/CardMenu.tsx
@@ -1,7 +1,8 @@
 import BaseCard from '@components/modules/BaseCard'
+import ICONS from '@configs/icons'
 import IMAGES from '@configs/images'
 import { CartContext } from '@contexts/CartContext/context'
-import { formatIDR } from '@utils/index'
+import { clsx, formatIDR } from '@utils/index'
 import type React from 'react'
 import { useContext } from 'react'
 import Skeleton from 'react-loading-skeleton'
@@ -14,7 +15,7 @@ type Props = {
 }
 
 export const CardMenu: React.FC<Props> = ({ data, loading }) => {
-  const { addProduct } = useContext(CartContext)
+  const { addProduct, products, minusQty, plusQty } = useContext(CartContext)
 
   const handleClick = (val: ProductList) => {
     addProduct({
@@ -36,32 +37,74 @@ export const CardMenu: React.FC<Props> = ({ data, loading }) => {
             className="object-cover h-40 rounded-t-2xl border-b border-b-border"
             src={IMAGES.Food}
           />
-          <Skeleton width={150} />
-          <Skeleton width={100} />
-          <Skeleton width={100} />
+          <div>
+            <Skeleton width={150} />
+            <Skeleton width={100} />
+            <Skeleton width={100} />
+          </div>
         </BaseCard>
       </div>
     ))
   }
-  return data?.map((item, index) => (
-    <div
-      className={styles.container}
-      key={index}
-      onClick={() => handleClick(item)}
-    >
-      <BaseCard className={styles.card}>
-        <img
-          alt="menu"
-          className="object-cover h-40 rounded-t-2xl border-b border-b-border"
-          src={item.img || IMAGES.Food}
-        />
+  return data?.map((item, index) => {
+    const ordData = products.filter(
+      (val) => val.productId === Number(item.id)
+    )[0]
+    const isAvail = item?.stock !== 0 ? ordData?.qty !== item.stock : false
+    return (
+      <div
+        className={clsx([
+          styles.container,
+          isAvail ? 'cursor-pointer' : 'cursor-not-allowed',
+        ])}
+        key={index}
+      >
+        <BaseCard className={styles.card}>
+          <img
+            alt="menu"
+            className="object-cover h-40 rounded-t-2xl border-b border-b-border"
+            src={item.img || IMAGES.Food}
+          />
 
-        <div>
-          <h1>{item.name}</h1>
-          <h2>{formatIDR(item.price)}</h2>
-          <p>Tersedia {item.stock}</p>
-        </div>
-      </BaseCard>
-    </div>
-  ))
+          <div>
+            <h1>{item.name}</h1>
+            <h2>{formatIDR(item.price)}</h2>
+            <p>Tersedia {item.stock}</p>
+          </div>
+
+          <div className="w-full">
+            {ordData ? (
+              <div className="flex items-center justify-between h-12">
+                <button onClick={() => minusQty(Number(item.id))}>
+                  <ICONS.MinusCircle height={30} width={30} />
+                </button>
+                <p className="font-semibold">{ordData?.qty || 0}</p>
+                <ICONS.PlusCircle
+                  height={30}
+                  onClick={() => plusQty(Number(item.id))}
+                  width={30}
+                />
+              </div>
+            ) : (
+              <button
+                className="text-sm p-3 border border-orange rounded-full w-full h-12"
+                disabled={!isAvail}
+                onClick={() => handleClick(item)}
+              >
+                Tambah
+              </button>
+            )}
+          </div>
+        </BaseCard>
+
+        {!isAvail ? (
+          <div className="absolute flex items-center justify-center top-0 left-0 bg-primary w-full h-full bg-opacity-25 rounded-2xl">
+            <div className="bg-primary bg-opacity-50 text-white text-center rounded-full px-4 py-6 text-sm">
+              Habis
+            </div>
+          </div>
+        ) : null}
+      </div>
+    )
+  })
 }
